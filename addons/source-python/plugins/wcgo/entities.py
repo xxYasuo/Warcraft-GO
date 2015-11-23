776b0ba8e0109e91f042066b32441552bfa1f5a5
MarkusMeskanen <markusmeskanen@gmail.com>
2015-11-23 15:38:59 +0200

    Added Entity base class into entities module

--- addons/source-python/plugins/wcgo/entities.py
+++ addons/source-python/plugins/wcgo/entities.py
@@ -1,1 +1,25 @@
 """Module for Warcraft: GO entities like heroes and skills."""
+
+
+from wcgo.utilities import ClassProperty
+
+
+class Entity:
+    """Base class for all entities."""
+
+    @ClassProperty
+    def clsid(cls):
+        """Get the class's ID."""
+        return cls.__name__
+
+    @ClassProperty
+    def name(cls):
+        """Get the class's name."""
+        return cls.__name__.replace('_', ' ')
+
+    @ClassProperty
+    def description(cls):
+        """Get the class's description."""
+        return cls.__doc__ if cls.__doc__ is not None else ''
+
+    authors = tuple()
