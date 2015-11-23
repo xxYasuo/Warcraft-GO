240994c3588c2ccc15534388c1d688a96e577565
MarkusMeskanen <markusmeskanen@gmail.com>
2015-11-23 15:48:57 +0200

    Added LevelableEntity class into entities module

--- addons/source-python/plugins/wcgo/entities.py
+++ addons/source-python/plugins/wcgo/entities.py
@@ -23,3 +23,27 @@ class Entity:
         return cls.__doc__ if cls.__doc__ is not None else ''
 
     authors = tuple()
+
+
+class LevelableEntity(Entity):
+    """Entity class which implements a level system."""
+
+    max_level = None
+
+    def __init__(self, level=0):
+        """Initialize a new entity with levels."""
+        self._level = level
+
+    @property
+    def level(self):
+        """Get the entity's level."""
+        return self._level
+
+    @level.setter
+    def level(self, value):
+        """Set the entity's level."""
+        if value < 0:
+            raise ValueError("Entity's level can't be negative")
+        if self.max_level is not None and value > self.max_level:
+            raise ValueError("Entity's level bigger than its max_level")
+        self._level = value
