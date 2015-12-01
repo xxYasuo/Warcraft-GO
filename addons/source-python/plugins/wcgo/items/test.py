0f61b5a3d8fbdb2fa37fd72cbb4c30467711dc8e
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2015-12-01 22:44:59 +0000

    Updated test item
    Upgraded the test item to use the new events provided by the menus, and
    added that the item gives speed again when spawning. Gets destroyed upon
    death.

--- addons/source-python/plugins/wcgo/items/test.py
+++ addons/source-python/plugins/wcgo/items/test.py
@@ -5,10 +5,15 @@ import wcgo.entities
 
 
 class Test_Item(wcgo.entities.Item):
-    'Just a test item.'
 
     cost = 300
     limit = 1
 
     def item_purchased(self, player, **eargs):
+        player.speed += 0.5
+
+    def item_sold(self, player, **eargs):
+        player.speed -= 0.5
+
+    def player_spawn(self, player, **eargs):
         player.speed += 0.5
