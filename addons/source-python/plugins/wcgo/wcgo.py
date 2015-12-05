e416c0dc84c3ae967f7c5861aecc0b537dbae434
MarkusMeskanen <markusmeskanen@gmail.com>
2015-12-05 14:11:44 +0200

    Revert "Updated pre_on_take_damage's active_weapon"
    This reverts commit 40be16e1a152f721e7f758fc206bac281be86bc6.

--- addons/source-python/plugins/wcgo/wcgo.py
+++ addons/source-python/plugins/wcgo/wcgo.py
@@ -327,8 +327,13 @@ def _pre_on_take_damage(args):
         'attacker': attacker,
         'victim': victim,
         'info': info,
-        'weapon': attacker.active_weapon,
     }
+    # Adds the weapon argument dependent on scenario
+    if not attacker is None and attacker.active_weapon != -1:
+        eargs['weapon'] = Weapon(index_from_inthandle(attacker.active_weapon))
+    else:
+        eargs['weapon'] = None
+
     if attacker is None or attacker.userid == victim.userid:
         victim.hero.execute_skills('player_pre_self_injury', player=victim, **eargs)
         return
