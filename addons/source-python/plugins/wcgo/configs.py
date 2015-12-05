65f01d5a87f48536238d8af012c1cf398cc672f9
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2015-12-05 01:08:23 +0000

    Created a debug hero
    Created a debug hero using all the features implemented so far.

--- addons/source-python/plugins/wcgo/configs.py
+++ addons/source-python/plugins/wcgo/configs.py
@@ -62,7 +62,7 @@ reset_skills_cost = 50
 
 # Starting heroes for when an user joins the server for the first time
 starting_heroes = (
-    'Undead_Scourge',
+    'Debug_Hero',
 )
 
 # Hero category to use when no category is defined
---
+++ addons/source-python/plugins/wcgo/heroes/debug.py
@@ -0,0 +1,76 @@
+"""Debug hero for wcgo testing."""
+
+# Python3
+from random import randint
+
+# Source.Python
+from messages import SayText2
+from listeners.tick import tick_delays
+
+# Warcraft: GO
+from wcgo.cooldown import cooldown
+from wcgo.configs import message_template
+from wcgo.entities import Hero
+from wcgo.entities import Skill
+
+# Message function
+def message(index, message, template=message_template):
+    instance = SayText2(template.format(message))
+    instance.send(index)
+
+# Create hero
+class Debug_Hero(Hero):
+    'Hero for debugging Warcraft: GO.'
+
+    authors = 'Predz', 'Mahi'
+    category = 'DEBUG'
+    cost = 0
+    max_level = 10
+
+# Create passive
+@Debug_Hero.passive
+class Health_Increase(Skill):
+    'Gain a health increase upon spawning.'
+
+    def player_spawn(self, player, **eargs):
+        player.health += 40
+
+# Create chance skill
+@Debug_Hero.skill
+class Lifesteal(Skill):
+    'Retrieve 60% of damage back as health, on chance.'
+
+    max_level = 8
+
+    def player_pre_attack(self, attacker, info, **eargs):
+        chance = 20 + (5 * self.level)
+        if randint(0, 100) <= chance:
+            health = info.damage * 0.6
+            attacker.health += health
+            message(attacker.index,
+                '\x02Stole {health} health from enemy'.format(health=health))
+
+# Create cooldown skill
+@Debug_Hero.skill
+class Longjump(Skill):
+    'Jump much further than normal.'
+
+    max_level = 8
+
+    @cooldown(3)
+    def player_jump(self, player, **eargs):
+        player.push(200 + (10 * self.level), 1, 150)
+
+# Create ultimate skill
+@Debug_Hero.skill
+class Burst_of_Speed(Skill):
+    'On ultimate you gain massive speed.'
+
+    max_level = 8
+
+    @cooldown(10)
+    def player_ultimate(self, player, **eargs):
+        speed = 1 + (0.1 * self.level)
+        player.speed *= speed
+        message(attacker.index,
+                '\x03You gained {speed}% more speed.'.format(speed=speed*100))
