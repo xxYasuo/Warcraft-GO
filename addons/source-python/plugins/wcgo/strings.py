d30b123dd6109df418338e3df204e2504394dbcf
Stephen Toon <satoon101@gmail.com>
2015-12-06 11:04:57 -0500

    Removed formatting of messages in strings.py and added the keyword arguments to the send call to be formatted by TranslationStrings itself.

--- addons/source-python/plugins/wcgo/strings.py
+++ addons/source-python/plugins/wcgo/strings.py
@@ -13,14 +13,13 @@ from wcgo.info import info
 MESSAGES = LangStrings(info.basename)
 
 
-def chat_message(player_index, msg_id, kwargs):
+def chat_message(player_index, msg_id, **kwargs):
     """Send a message to a player using SayText2."""
-    msg = MESSAGES[msg_id].format(**kwargs)
-    msg = cfg.message_template.format(msg)
-    SayText2(msg).send(player_index)
+    msg = MESSAGES[msg_id]
+    SayText2(msg).send(player_index, **kwargs)
 
 
-def hint_message(player_index, msg_id, kwargs):
+def hint_message(player_index, msg_id, **kwargs):
     """Send a message to a player using HintText."""
-    msg = MESSAGES[msg_id].format(**kwargs)
-    HintText(msg).send(player_index)
+    msg = MESSAGES[msg_id]
+    HintText(msg).send(player_index, **kwargs)
