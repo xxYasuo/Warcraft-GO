0ad8b225ba89e5f22cd47d220576ee0b73c1b74a
MarkusMeskanen <markusmeskanen@gmail.com>
2015-12-06 14:26:16 +0200

    Renamed item's events
    item_sold is now item_sell and item_purchased is now item_purchase
    This is now consistent with Source engine's style.

--- addons/source-python/plugins/wcgo/menus/items.py
+++ addons/source-python/plugins/wcgo/menus/items.py
@@ -29,7 +29,7 @@ def _item_sell_menu_select(menu, index, choice):
     player = Player(index)
     item = choice.value
     player.cash += item.sell_value
-    item.execute_method('item_sold', item=item, player=player)
+    item.execute_method('item_sell', item=item, player=player)
     player.hero.items.remove(item)
     strings.message(index, 'Sell Item Success', item=item.name, cost=item.sell_value)
 
@@ -59,7 +59,7 @@ def _item_buy_menu_select(menu, index, choice):
     if player.cash >= item.cost:
         item = item()
         player.hero.items.append(item)
-        item.execute_method('item_purchased', item=item, player=player)
+        item.execute_method('item_purchase', item=item, player=player)
         player.cash -= item.cost
         strings.message(index, 'Buy Item Success', item=item.name, cost=item.cost)
     else:
