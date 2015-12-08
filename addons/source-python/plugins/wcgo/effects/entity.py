d23f327e46f7f7a067a6bfe814072938ce98b1d6
Markus Meskanen <markusmeskanen@gmail.com>
2015-12-09 00:41:57 +0200

    Updated Entity's docstrings
    Added docstrings to all methods. Also [PEP 0257](https://www.python.org/dev/peps/pep-0257/) compliant.

--- addons/source-python/plugins/wcgo/effects/entity.py
+++ addons/source-python/plugins/wcgo/effects/entity.py
@@ -5,19 +5,23 @@ import entities
 
 
 class Entity(entities.entity.Entity):
-    '''Subclassing Entity to ensure input and outputs
-    are never called when entity doesn't exist.'''
+    """Entity with safe methods to make sure the entity exists."""
+    
     def __init__(self, index):
+        """Initialize a new entity and store his ehandle."""
         super().__init__(index)
         self._ehandle = self.get_ref_ehandle()
 
     def is_valid(self):
+        """Use the ehandle to see if the entity is still valid."""
         return self._ehandle.is_valid()
 
     def add_output_safe(self, statement):
+        """Safely add an output to the entity."""
         if self.is_valid():
             self.add_output(statement)
 
     def call_input_safe(self, statement):
+        """Safely call an input on the entity."""
         if self.is_valid():
             self.call_input(statement)
