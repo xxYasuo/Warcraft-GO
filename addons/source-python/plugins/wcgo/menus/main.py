7eb20c651cfe4ee4bf7d5e9bfb5a7e9a0615e67c
MarkusMeskanen <markusmeskanen@gmail.com>
2015-12-11 00:30:54 +0200

    Updated all menus to use the new translations

--- addons/source-python/plugins/wcgo/menus/main.py
+++ addons/source-python/plugins/wcgo/menus/main.py
@@ -1,12 +1,14 @@
 ﻿"""Provides the main menu instance."""
 
 # Source.Python
-import menus
+from menus import SimpleMenu
+from menus import SimpleOption
+from menus import Text
 
 # Warcraft: GO
 import wcgo.configs
 import wcgo.player
-import wcgo.strings
+from wcgo.strings import menu_options
 from wcgo.menus.heroes import current_hero_menu
 from wcgo.menus.heroes import owned_categories_menu
 from wcgo.menus.heroes import buy_categories_menu
@@ -15,15 +17,11 @@ from wcgo.menus.items import item_sell_menu
 from wcgo.menus.players import player_list_menu
 
 
-# Menu translations
-_tr = wcgo.strings.menus['main']
-
-
 # Main menu instance
 
 def _main_menu_build(menu, index):
     player = wcgo.player.Player(index)
-    menu[1].text = _tr['Gold'].get_string(gold=player.gold)
+    menu[1].text = menu_options['Gold'].get_string(gold=player.gold)
 
 
 def _main_menu_select(menu, index, choice):
@@ -33,21 +31,21 @@ def _main_menu_select(menu, index, choice):
         return next_menu
 
 
-main_menu = menus.SimpleMenu(
+main_menu = SimpleMenu(
     [
-        menus.Text(_tr['Title']),
-        menus.Text(' '),
-        menus.Text(wcgo.configs.menu_separator),
-        menus.SimpleOption(1, _tr['Current Hero'], current_hero_menu),
-        menus.SimpleOption(2, _tr['Owned Heroes'], owned_categories_menu),
-        menus.SimpleOption(3, _tr['Buy Heroes'], buy_categories_menu),
-        menus.Text(wcgo.configs.menu_separator),
-        menus.SimpleOption(4, _tr['Buy Items'], item_categories_menu),
-        menus.SimpleOption(5, _tr['Sell Items'], item_sell_menu),
-        menus.Text(wcgo.configs.menu_separator),
-        menus.SimpleOption(6, _tr['Player Info'], player_list_menu),
-        menus.Text(wcgo.configs.menu_separator),
-        menus.SimpleOption(9, wcgo.strings.menus['defaults']['Close'], highlight=False)
+        Text(menu_options['Main Menu']),
+        Text(' '),
+        Text(wcgo.configs.menu_separator),
+        SimpleOption(1, menu_options['Current Hero'], current_hero_menu),
+        SimpleOption(2, menu_options['Owned Heroes'], owned_categories_menu),
+        SimpleOption(3, menu_options['Buy Heroes'], buy_categories_menu),
+        Text(wcgo.configs.menu_separator),
+        SimpleOption(4, menu_options['Buy Items'], item_categories_menu),
+        SimpleOption(5, menu_options['Sell Items'], item_sell_menu),
+        Text(wcgo.configs.menu_separator),
+        SimpleOption(6, menu_options['Player Info'], player_list_menu),
+        Text(wcgo.configs.menu_separator),
+        SimpleOption(9, menu_options['Close'], highlight=False),
     ],
     build_callback=_main_menu_build,
     select_callback=_main_menu_select)
